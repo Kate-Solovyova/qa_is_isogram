7cd9f0c2fe3b644a1faab9f3ef2e891dbee8e3e8
Kate Bass <katebass@MacBook-Pro-Dmitry.local>
2023-02-05 20:46:39 +0200

    added tests

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -6,4 +6,30 @@ describe('isIsogram', () => {
   it(`should be declared`, () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
+
+  it(`should be declared`, () => {
+    const result = isIsogram('');
+    expect(typeof result === 'boolean')
+      .toBeTruthy();
+  });
+
+  it(`if input is empty string, should return true`, () => {
+    expect(isIsogram(''))
+      .toBe(true);
+  });
+
+  it(`if input has two the same letters, should return false`, () => {
+    expect(isIsogram('look'))
+      .toBe(false);
+  });
+
+  it(`if the input has each letter occurs 1 time, should return true`, () => {
+    expect(isIsogram('playgrounds'))
+      .toBe(true);
+  });
+
+  it(`if input has two or more letters in different cases, should return false`, () => {
+    expect(isIsogram('Adam'))
+      .toBe(false);
+  });
 });
